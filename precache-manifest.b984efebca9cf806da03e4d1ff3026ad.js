e80aa5a2df2141bca00462690f36a0083d805392
raymondfdavey <raymondfdavey@gmail.com>
2020-04-01 10:05:34 +0100

    Updates

---
+++ precache-manifest.b984efebca9cf806da03e4d1ff3026ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33ac433eca38f1d1a4250b290944acb",
+    "url": "/yrglset/index.html"
+  },
+  {
+    "revision": "5a9d26ada4119490931f",
+    "url": "/yrglset/static/css/main.d46bdd8d.chunk.css"
+  },
+  {
+    "revision": "7acfefc9938dc19d7b0c",
+    "url": "/yrglset/static/js/2.e4dc2ec2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/yrglset/static/js/2.e4dc2ec2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a9d26ada4119490931f",
+    "url": "/yrglset/static/js/main.0a29c2e5.chunk.js"
+  },
+  {
+    "revision": "71df5e2718b3920b15f4",
+    "url": "/yrglset/static/js/runtime-main.d514da7e.js"
+  }
+]);
